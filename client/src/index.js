fc95b6950fe4f7dd4de1553dea8752ef9cd28f6c
Braxton McClellan <thebraxmc@gmail.com>
2020-10-05 19:00:01 -0600

    init

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { initMiddleware } from 'devise-axios';
+// auth stuff
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
