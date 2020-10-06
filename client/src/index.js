143894102d3764b29bf71e5d65c716fb41b602d7
Braxton McClellan <thebraxmc@gmail.com>
2020-10-05 20:13:06 -0600

    end of lecture

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { initMiddleware } from 'devise-axios';
-// auth stuff
+import AuthProvider from './providers/AuthProvider';
+
+initMiddleware();
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
